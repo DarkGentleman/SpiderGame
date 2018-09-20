b4feb54a70e6c63baa95a2fda8173066b760f6ce
DarkGentleMan <42516780+DarkGentleman@users.noreply.github.com>
2018-09-20 12:42:32 +0800

    Update 股票数据定向爬取.py

--- 股票数据定向爬取.py
+++ 股票数据定向爬取.py
@@ -55,7 +55,7 @@ def getStockInfo(lst, stockURL, fpath):
 def main():
     stock_list_url = 'http://quote.eastmoney.com/stocklist.html'
     stock_info_url = 'https://gupiao.baidu.com/stock/'
-    output_file = '/home/cai/stockinfolist.py'
+    output_file = '/home/cai/stockinfolist.txt'
     slist = []
     getStockList(slist, stock_list_url)
     getStockInfo(slist, stock_info_url, output_file)
